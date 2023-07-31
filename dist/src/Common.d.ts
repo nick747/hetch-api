9cb372785339b8af15ce6726d6da59ff91478441
nick747 <micco.mario@gmail.com>
2023-07-31 18:15:59 +0200

    Cleared cache

--- dist/src/Common.d.ts
+++
@@ -1,5 +0,0 @@
-import { RequestConfig } from './types';
-/**
- * Default settings for config in `Hetch` and `Base` classes.
- */
-export declare let ConfigDefaults: RequestConfig;
--- dist/tests/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sampleCode_1 = require("./sampleCode");
-(0, sampleCode_1.testHetch)();
