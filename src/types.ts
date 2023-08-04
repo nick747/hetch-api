aa3f3c9413cfe96a411e8506cbbf04df1694c133
nick747 <micco.mario@gmail.com>
2023-08-04 21:35:41 +0200

    fix: tracked .eslintrc.js

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,30 @@
 /* --- */
 export type Interceptor = {
-    <T>(responseData: T): Promise<T> | T;
-    <T>(responseData: T, response: Response): Promise<T> | T;
-  };
-
-/* --- */
-export interface RequestConfig extends Omit<RequestInit, 'body'> {
-    customHeaders?: Headers | string[][];
-    timeout?: number;
-    maxRetries?: number; // the maximum number of retries for network errors
-    retryDelay?: number; // the delay between each try
-    body?: string | object | null;
+  <T>(responseData: T): Promise<T> | T;
+  <T>(responseData: T, response: Response): Promise<T> | T;
 };
 
 /* --- */
-export type ResponseStructure = {
-    status?: number,
-    working?: string | boolean | null,
-    userId?: number,
-    id?: number,
-    title?: string,
-    message?: string,
-    jsonData?: object;
+export interface RequestConfig extends Omit<RequestInit, "body"> {
+  customHeaders?: Headers | string[][];
+  timeout?: number;
+  maxRetries?: number; // the maximum number of retries for network errors
+  retryDelay?: number; // the delay between each try
+  body?: string | object | null;
 }
 
 /* --- */
+export type ResponseStructure = {
+  status?: number;
+  working?: string | boolean | null;
+  userId?: number;
+  id?: number;
+  title?: string;
+  message?: string;
+  jsonData?: object;
+};
+
+/* --- */
 export type ResponseData = {
-    data: object | string;
-}
+  data: object | string;
+};
